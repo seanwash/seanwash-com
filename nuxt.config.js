712fd0b482fe4c527173381da00a8aafa73131f5
Sean Washington <hello@seanwash.com>
2018-07-04 15:59:33 -0700

    Updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,6 @@ require('dotenv').config()
 module.exports = {
   mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
   head: {
     title: 'Sean Washington',
     meta: [
@@ -19,52 +16,40 @@ module.exports = {
         hid: 'description',
         name: 'description',
         content: 'Podcaster, software engineer, regular human being.'
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Podcaster, software engineer, regular human being.'
+      },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content: 'https://seanwash.com/og-image.jpg'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
 
   env: {
     GA_ID: process.env.GA_ID
   },
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#FFFFFF' },
+  loading: { color: '#000' },
 
-  /*
-  ** Global CSS
-  */
   css: ['@/assets/css/poole.css', '@/assets/css/hyde.css'],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
   plugins: [{ src: '~plugins/ga.js', ssr: false }],
-
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     '@nuxtjs/sitemap'
   ],
-  /*
-  ** Axios module configuration
-  */
+
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
-  /*
-  ** Build configuration
-  */
   build: {
-    /*
-    ** You can extend webpack config here
-    */
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
